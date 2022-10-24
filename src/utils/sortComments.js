e93361cd7d67674084db84a27b8cd952ae7515d5
Alimardon <alimardon007@gmail.com>
2022-10-24 18:17:29 +0300

    fix sort function for null comments

--- src/utils/sortComments.js
+++ src/utils/sortComments.js
@@ -1,6 +1,10 @@
 export const sortComments = (comments) => {
-    const copyComments = [...comments];
-    return copyComments
-        .filter(Boolean)
-        .sort(({ time: a }, { time: b }) => (a > b ? 1 : a < b ? -1 : 0));
+    if (comments !== undefined) {
+        const copyComments = [...comments];
+        return copyComments
+            .filter(Boolean)
+            .sort(({ time: a }, { time: b }) => (a > b ? 1 : a < b ? -1 : 0));
+    } else {
+        return comments;
+    }
 };
